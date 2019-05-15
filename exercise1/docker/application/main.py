cd8c58ac8c9ddd951ce399a620810607cdca9482
EC2 Default User <ec2-user@ip-172-31-45-31.eu-west-1.compute.internal>
2019-05-15 15:46:33 +0000

    Today's work, saved

--- exercise1/docker/application/main.py
+++ exercise1/docker/application/main.py
@@ -1,3 +1,7 @@
+from prometheus_client import Counter
+
+c = Counter('covinha_count', 'Covinha site hit count')
 def application(environ, start_fn):
     start_fn('200 OK', [('Content-Type', 'text/plain')])
+    c.inc()
     return ["Hello World!\n"]
---
+++ exercise1/docker/prestart.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+# Run custom Python script before starting
+python /app/start_prometheus.py
